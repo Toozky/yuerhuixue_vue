46afcfe7a9bff4c4205453131d23aae30f826221
ZhangYu <toozky@foxmail.com>
2021-10-19 15:39:26 +0800

    悦耳慧学前端

--- src/views/AdminMain.vue
+++ src/views/AdminMain.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <div id="GenderCount">
+  <div id="admin-main">
+    <div id="gender-count">
+
+    </div>
+    <div id="people-number">
 
     </div>
   </div>
@@ -21,24 +24,30 @@ export default {
       },
 
 
+      //用户数统计表
+      userAndAdminTotal: {
+        adminTotal: 1,
+        userTotal: 1
+      }
+
     }
   },
 
   methods: {
-    getUserGenderEcharts() {
+    getUserGenderOption() {
       //用户性别统计echarts参数
-      var userGenderOption= {
+      var userGenderOption = {
         title: {
-          text: '用户性别人数统计',
-              subtext: '',
-              left: 'center'
+          text: '用户性别人数画像',
+          subtext: '',
+          left: 'center'
         },
         tooltip: {
           trigger: 'item'
         },
         legend: {
           orient: 'vertical',
-              left: 'left'
+          left: 'left'
         },
         series: [
           {
@@ -61,33 +70,81 @@ export default {
         ]
       };
 
-      // 获取dom对象并初始化
-      let userGenderChart = this.$echarts.init(
-          document.getElementById("GenderCount")
-      );
-
-      //设置option
-      userGenderChart.setOption(userGenderOption)
+      return userGenderOption
     },
+
+    getPeopleNumberOption() {
+      //管理员用户人数统计echarts参数
+      var peopleNumberOption = {
+        title: {
+          text: '人数统计（权限）',
+          subtext: '',
+          left: 'center'
+        },
+        xAxis: {
+          data: ['管理员', '用户']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [this.userAndAdminTotal.adminTotal, this.userAndAdminTotal.userTotal],
+            type: 'bar'
+          }
+        ]
+      };
+      return peopleNumberOption
+    },
+
+    eChartInitSetOption() {
+      let genderCountEchart = this.$echarts.init(
+          document.getElementById("gender-count"),
+      )
+      let peopleNumberEchart = this.$echarts.init(
+          document.getElementById("people-number"),
+      )
+
+      genderCountEchart.setOption(this.getUserGenderOption())
+      peopleNumberEchart.setOption(this.getPeopleNumberOption())
+
+    }
   },
 
   created() {
     const _this = this
-    //获取用户性别人数统计
+
+    // 获取用户性别人数统计
     axios.get('/admin/userGenderTotal').then((resp) => {
       _this.userGenderTotal = resp.data.data
-      console.log(_this.userGenderTotal)
-      _this.getUserGenderEcharts()
+      _this.eChartInitSetOption()
+    });
+
+    //获取人数统计（权限）
+    axios.get('/admin/userAndAdminTotal').then((resp) => {
+      _this.userAndAdminTotal = resp.data.data
+      _this.eChartInitSetOption()
     });
 
 
-  }
+  },
+
 }
 </script>
 
 <style scoped>
-#GenderCount{
-  width: 500px;
-  height: 500px
+#admin-main {
+
+}
+
+#gender-count, #people-number {
+  margin-top: 50px;
+  float: left;
+  width: 700px;
+  height: 700px
+}
+
+#people-number {
+  margin-left: 100px;
 }
 </style>
--- src/views/AdminShopMain.vue
+++ src/views/AdminShopMain.vue
@@ -1,15 +1,161 @@
 <template>
-<div>
-  <h1>这是AdminShopMain</h1>
-</div>
+  <div id="shop-main">
+    <div id="ins-total-type">
+
+    </div>
+    <div id="ins-most-expensive">
+
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "AdminShopMain"
+  name: "AdminShopMain",
+
+  data(){
+    return {
+      //各乐器类型乐器个数
+      insTotalByType:[
+        { value: 40, name: 'rose 1' },
+        { value: 38, name: 'rose 2' },
+        { value: 32, name: 'rose 3' },
+        { value: 30, name: 'rose 4' },
+        { value: 28, name: 'rose 5' },
+      ],
+      //最贵乐器top5
+      expensiveIns:[
+        ['a', 'b', 'c', 'd', 'e'],
+        [120, 200, 150, 80, 70]
+      ],
+
+    }
+  },
+
+  methods:{
+    getInsTotalByType(){
+      var insTotalByTypeOption = {
+        title: {
+          text: '各类型乐器占比',
+          subtext: '',
+          left: 'center'
+        },
+        legend: {
+          top: 'bottom'
+        },
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        series: [
+          {
+            name: 'Nightingale Chart',
+            type: 'pie',
+            radius: [50, 250],
+            center: ['50%', '50%'],
+            roseType: 'area',
+            itemStyle: {
+              borderRadius: 8
+            },
+            data:
+            this.insTotalByType
+          }
+        ]
+      };
+
+      return insTotalByTypeOption;
+    },
+
+    getExpensiveInsOption(){
+      //获取最贵乐器TOP5 echarts参数
+      var expensiveInsOption = {
+        title: {
+          text: '最贵乐器TOP5',
+          subtext: '',
+          left: 'center'
+        },
+        dataset: [
+          {
+            dimensions: ['typeName',  'price'],
+            source: [
+              [this.expensiveIns[0][0],this.expensiveIns[1][0]],
+              [this.expensiveIns[0][1],this.expensiveIns[1][1]],
+              [this.expensiveIns[0][2],this.expensiveIns[1][2]],
+              [this.expensiveIns[0][3],this.expensiveIns[1][3]],
+              [this.expensiveIns[0][4],this.expensiveIns[1][4]],
+            ]
+          },
+          {
+            transform: {
+              type: 'sort',
+              config: { dimension: 'price', order: 'desc' }
+            }
+          }
+        ],
+        xAxis: {
+          type: 'category',
+          axisLabel: { interval: 0, rotate: 30 }
+        },
+        yAxis: {},
+        series: {
+          type: 'bar',
+          encode: { x: 'typeName', y: 'price' },
+          datasetIndex: 1
+        }
+      };
+
+      return expensiveInsOption;
+    },
+
+    eChartInitSetOption() {
+      let expensiveInsEchart = this.$echarts.init(
+          document.getElementById("ins-most-expensive"),
+      )
+      let insTotalByTypeEchart = this.$echarts.init(
+          document.getElementById("ins-total-type"),
+      )
+
+      expensiveInsEchart.setOption(this.getExpensiveInsOption())
+      insTotalByTypeEchart.setOption(this.getInsTotalByType())
+
+    }
+  },
+
+  created() {
+    const _this = this
+    //获取各种乐器类型的乐器个数
+    axios.get('/insType/insTotalByType').then((resp) => {
+      _this.insTotalByType=resp.data.data
+      _this.eChartInitSetOption();
+    });
+
+
+    // 获取最贵乐器TOP5
+    axios.get('/ins/mostFiveExpensive').then((resp) => {
+      _this.expensiveIns=resp.data.data
+      _this.eChartInitSetOption();
+    });
+  }
+
 }
 </script>
 
 <style scoped>
+#shop-main {
 
+}
+
+#ins-total-type,#ins-most-expensive  {
+  margin-top: 50px;
+  float: left;
+  width: 700px;
+  height: 700px
+}
+
+#ins-most-expensive {
+  margin-left: 100px;
+}
 </style>
