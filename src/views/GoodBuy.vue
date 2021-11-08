c0ecc913ae9c6eb1e3e132bc34fef27b61ace343
ZhangYu <toozky@foxmail.com>
2021-11-08 11:47:02 +0800

    购买，购物车

--- src/views/GoodBuy.vue
+++ src/views/GoodBuy.vue
@@ -23,11 +23,12 @@
           <br>
           <br>
           <div>
-            <a style="margin:5px 0;line-height: 30px;cursor: pointer" @click="toInsTypePage(insType.typeId)">类型　{{ insType.typeName }}</a><br>
+            <a style="margin:5px 0;line-height: 30px;cursor: pointer"
+               @click="toInsTypePage(insType.typeId)">类型　{{ insType.typeName }}</a><br>
             <a style="margin:5px 0;line-height: 30px">品牌　{{ instrument.insBrand }}</a><br>
             <a style="margin:5px 0;line-height: 30px">库存　{{ instrument.insStock }}　个</a><br>
           </div>
-          <div style="margin-top: 20px;float: left" >
+          <div style="margin-top: 20px;float: left">
             <el-dropdown>
   <span class="el-dropdown-link" style="font-size: medium">
     配送至<i class="el-icon-arrow-down el-icon--right"></i>
@@ -37,18 +38,21 @@
                     v-for="address in userAddr"
                     :index="address.addrId"
                     :key="address.addrId"
-                @click.native="ckeckAddr(address.receiverAddr)">
+                    @click.native="
+                      ckeckAddr(address.receiverAddr),
+                      receiverNameChecked=address.receiverName,
+                      receiverTelChecked=address.receiverTel">
                   {{ address.receiverAddr }}
                 </el-dropdown-item>
-                <el-dropdown-item>    </el-dropdown-item>
+                <el-dropdown-item></el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
           </div>
-          <div style="margin: 20px">　{{receiverAddrChecked}}</div>
+          <div style="margin: 20px">　{{ receiverAddrChecked }}</div>
         </div>
 
         <div id="insBuyBtn">
-          <el-button type="danger">　购买　</el-button>
+          <el-button type="danger" @click="buyGoods">　购买　</el-button>
           <el-button type="success" @click="addShopCart(shoppingCart)">加入购物车</el-button>
         </div>
 
@@ -121,19 +125,44 @@ export default {
           receiverAddr: '',
         },
       ],
-      receiverAddrChecked:'',
+      receiverAddrChecked: '',
+      receiverNameChecked: '',
+      receiverTelChecked: '',
       //购物车信息
-      shoppingCart:{
-        cartId:null,
-        cartNumber:1,
-        userId:null,
-        createTime:null,
-        ins:{
-          insId:null,
-          insName:'',
-          insImg:'',
+      shoppingCart: {
+        cartId: null,
+        cartNumber: 1,
+        userId: null,
+        createTime: null,
+        ins: {
+          insId: null,
+          insName: '',
+          insImg: '',
         }
       },
+      //订单表
+      orders: {
+        orderId: null,
+        userId: null,
+        allGoods: '',
+        receiverName: '',
+        receiverTel: '',
+        receiverAddr: '',
+        totalAmount: 1,
+        actualAmount: 1,
+        payType: '1',
+        orderRemark: '',
+        status: '4',
+        createTime: null,
+        updateTime: null,
+        payTime: null,
+        deliverTime: null,
+        receiptTime: null,
+        finishTime: null,
+      },
+
+      //订单明细
+      orderDetail: {},
     }
   },
 
@@ -166,9 +195,9 @@ export default {
     },
 
     //选中收货地址
-    ckeckAddr(text){
-      const _this=this
-      _this.receiverAddrChecked=text
+    ckeckAddr(text) {
+      const _this = this
+      _this.receiverAddrChecked = text
     },
 
     //根据乐器类型id跳转至乐器类型页
@@ -183,23 +212,74 @@ export default {
     },
 
     //加入购物车
-    addShopCart(shoppingCart){
-      const _this=this
-      shoppingCart.userId=_this.form.userId
-      shoppingCart.ins.insId=_this.insId
-      shoppingCart.ins.insName=_this.instrument.insName
-      shoppingCart.ins.insImg=_this.instrument.insImg
+    addShopCart(shoppingCart) {
+      const _this = this
+      shoppingCart.userId = _this.form.userId
+      shoppingCart.ins.insId = _this.insId
+      shoppingCart.ins.insName = _this.instrument.insName
+      shoppingCart.ins.insImg = _this.instrument.insImg
       axios({
         method: 'post',
         url: '/shoppingCart/add',
         data: {
-          cartId:shoppingCart.cartId,
-          insId:shoppingCart.ins.insId,
-          insName:shoppingCart.ins.insName,
-          insImg:shoppingCart.ins.insImg,
-          cartNumber:shoppingCart.cartNumber,
-          userId:shoppingCart.userId,
-          createTime:shoppingCart.createTime,
+          cartId: shoppingCart.cartId,
+          insId: shoppingCart.ins.insId,
+          insName: shoppingCart.ins.insName,
+          insImg: shoppingCart.ins.insImg,
+          cartNumber: shoppingCart.cartNumber,
+          userId: shoppingCart.userId,
+          createTime: shoppingCart.createTime,
+        }
+      }).then((resp) => {
+        if (resp.data.code === 10000) {
+          _this.$message({
+            showClose: true,
+            message: resp.data.msg,
+            type: 'success'
+          });
+
+        }
+        if (resp.data.code === 10001) {
+          _this.$message({
+            showClose: true,
+            message: resp.data.msg,
+            type: 'error'
+          });
+        }
+      });
+    },
+    //购买
+    buyGoods() {
+      const _this = this
+      let orders = _this.orders
+      orders.userId = _this.form.userId
+      orders.allGoods = _this.instrument.insName
+      orders.receiverName = _this.receiverNameChecked
+      orders.receiverAddr = _this.receiverAddrChecked
+      orders.receiverTel=_this.receiverTelChecked
+
+
+      axios({
+        method: 'post',
+        url: '/order/addOrder',
+        data: {
+          orderId: orders.orderId,
+          userId: orders.userId,
+          allGoods: orders.allGoods,
+          receiverName: orders.receiverName,
+          receiverTel: orders.receiverTel,
+          receiverAddr: orders.receiverAddr,
+          totalAmount: orders.totalAmount,
+          actualAmount: orders.actualAmount,
+          payType: orders.payType,
+          orderRemark: orders.orderRemark,
+          status: orders.status,
+          createTime: orders.createTime,
+          updateTime: orders.updateTime,
+          payTime: orders.payTime,
+          deliverTime: orders.deliverTime,
+          receiptTime: orders.receiptTime,
+          finishTime: orders.finishTime,
         }
       }).then((resp) => {
         if (resp.data.code === 10000) {
@@ -251,7 +331,7 @@ export default {
         }
       }).then((resp) => {
         // console.log(resp.data)
-        _this.userAddr=resp.data.data
+        _this.userAddr = resp.data.data
       });
     } /*else {
       this.$router.push('/UserLogin');
--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -17,9 +17,9 @@
               <img style="width:160px;height:160px" :src="insImgUrl+cart.ins.insImg">
             </div>
             <div style="float: left;width: 650px;">
-              名称：{{cart.ins.insName}}<br>
-              单价：{{cart.ins.insPrice}}<br>
-              总价：{{cart.ins.insPrice*cart.cartNumber}}<br>
+              名称：{{ cart.ins.insName }}<br>
+              单价：{{ cart.ins.insPrice }}<br>
+              总价：{{ cart.ins.insPrice * cart.cartNumber }}<br>
             </div>
             <div style="float:left;line-height: 150px">
               数量：
@@ -28,7 +28,7 @@
                            @click="--cart.cartNumber,modifyCart(cart.cartId,cart.cartNumber)"
                            circle
                 ></el-button>
-                <el-button style="width:80px">{{cart.cartNumber}}</el-button>
+                <el-button style="width:80px">{{ cart.cartNumber }}</el-button>
                 <el-button icon="el-icon-plus"
                            @click="++cart.cartNumber,modifyCart(cart.cartId,cart.cartNumber)"
                            circle></el-button>
@@ -52,6 +52,42 @@
               @size-change="handleSizeChange">
           </el-pagination>
           <br>
+        </div>
+
+      </el-card>
+    </div>
+
+    <div id="buyerInfo">
+      <el-card>
+        <div style="float:left;width:1000px;">
+          <div style="margin-top: 20px;">
+            <el-dropdown>
+  <span class="el-dropdown-link" style="font-size: medium;margin-left: 20px;margin-bottom: 20px">
+    配送至<i class="el-icon-arrow-down el-icon--right"></i><br>
+  </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item
+                    v-for="address in userAddr"
+                    :index="address.addrId"
+                    :key="address.addrId"
+                    @click.native="
+                      ckeckAddr(address.receiverAddr),
+                      receiverNameChecked=address.receiverName,
+                      receiverTelChecked=address.receiverTel">
+                  {{ address.receiverAddr }}
+                </el-dropdown-item>
+                <el-dropdown-item></el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+          <br>
+          <div style="margin: 20px">收货人：{{ receiverNameChecked }}</div>
+          <div style="margin: 20px">电话：{{ receiverTelChecked }}</div>
+          <div style="margin: 20px">收货地址：{{ receiverAddrChecked }}</div>
+        </div>
+
+        <div style="float: left">
+          <el-button style="margin-top:80px" type="success" @click="buyGoods" round>购买</el-button>
         </div>
 
       </el-card>
@@ -124,6 +160,54 @@ export default {
       //分页 ↑
       //第一页 点击分类菜单调用pageNum（不写这个变量，方法里直接传1也可以）
       pageNum: 1,
+
+      //用户地址表
+      userAddr: [
+        {
+          addrId: '',
+          userId: '',
+          receiverName: '',
+          receiverTel: '',
+          receiverAddr: '',
+        },
+      ],
+      receiverAddrChecked: '',
+      receiverNameChecked: '',
+      receiverTelChecked: '',
+
+      //订单表
+      orders: {
+        orderId: null,
+        userId: null,
+        allGoods: '',
+        receiverName: '',
+        receiverTel: '',
+        receiverAddr: '',
+        totalAmount: 1,
+        actualAmount: 1,
+        payType: '1',
+        orderRemark: '',
+        status: '4',
+        createTime: null,
+        updateTime: null,
+        payTime: null,
+        deliverTime: null,
+        receiptTime: null,
+        finishTime: null,
+      },
+
+      //订单明细
+      orderDetail: {
+        detailId: null,
+        orderId: null,
+        insId: null,
+        insName: '',
+        insImg: '',
+        insPrice: '',
+        buyNumber: '',
+        totalAmount: '',
+      },
+      orderDetailList: [],
     }
   },
   methods: {
@@ -141,7 +225,7 @@ export default {
       _this.page(_this.currentPage, val)
     },
     //获取购物车列表
-    getShoppingCartList(pageNum, pageSize){
+    getShoppingCartList(pageNum, pageSize) {
       const _this = this
       axios({
         method: 'get',
@@ -149,7 +233,7 @@ export default {
         params: {
           pageNum: pageNum,
           pageSize: pageSize,
-          userId:_this.form.userId,
+          userId: _this.form.userId,
         }
       }).then((resp) => {
         if (resp.data.code === 10000) {
@@ -168,7 +252,7 @@ export default {
     },
 
     //修改购物车商品数量
-    modifyCart(cartId,cartNumber){
+    modifyCart(cartId, cartNumber) {
       const _this = this
       axios({
         method: 'put',
@@ -193,7 +277,7 @@ export default {
     },
 
     //删除购物车
-    deleteCart(cartId){
+    deleteCart(cartId) {
       const _this = this
       axios({
         method: 'delete',
@@ -213,7 +297,121 @@ export default {
           });
         }
       });
-    }
+    },
+
+    //选中收货地址
+    ckeckAddr(text) {
+      const _this = this
+      _this.receiverAddrChecked = text
+    },
+
+    //购买
+    buyGoods() {
+      const _this = this
+      let orders = _this.orders
+      orders.userId = _this.form.userId
+      // orders.allGoods = _this.shoppingCartList
+
+      let shoppingCartList = _this.shoppingCartList
+
+      let allGoods = ''
+      let totalAmount = 0
+      let actualAmount = 0
+      for (let j = 0; j < shoppingCartList.length; j++) {
+        allGoods += shoppingCartList[j].ins.insName + ','
+        totalAmount += shoppingCartList[j].ins.insPrice * shoppingCartList[j].cartNumber
+      }
+      actualAmount = totalAmount
+      orders.allGoods = allGoods
+      orders.actualAmount=actualAmount
+      orders.totalAmount=totalAmount
+      orders.receiverName = _this.receiverNameChecked
+      orders.receiverAddr = _this.receiverAddrChecked
+      orders.receiverTel = _this.receiverTelChecked
+
+      console.log(orders)
+
+      axios({
+        method: 'post',
+        url: '/order/addOrder',
+        data: {
+          orderId: orders.orderId,
+          userId: orders.userId,
+          allGoods: orders.allGoods,
+          receiverName: orders.receiverName,
+          receiverTel: orders.receiverTel,
+          receiverAddr: orders.receiverAddr,
+          totalAmount: orders.totalAmount,
+          actualAmount: orders.actualAmount,
+          payType: orders.payType,
+          orderRemark: orders.orderRemark,
+          status: orders.status,
+          createTime: orders.createTime,
+          updateTime: orders.updateTime,
+          payTime: orders.payTime,
+          deliverTime: orders.deliverTime,
+          receiptTime: orders.receiptTime,
+          finishTime: orders.finishTime,
+        }
+      }).then((resp) => {
+        if (resp.data.code === 10000) {
+          //主订单id
+          // console.log(resp.data.data)
+
+          let shoppingCartList = _this.shoppingCartList
+          for(let i = 0; i < shoppingCartList.length; i++){
+            let orderDetail = new Object
+            orderDetail.detailId= 0
+            orderDetail.orderId=resp.data.data
+            orderDetail.insId=shoppingCartList[i].ins.insId
+            orderDetail.insName=shoppingCartList[i].ins.insName
+            orderDetail.insImg=shoppingCartList[i].ins.insImg
+            orderDetail.insPrice=shoppingCartList[i].ins.insPrice
+            orderDetail.buyNumber=shoppingCartList[i].cartNumber
+            orderDetail.totalAmount=orderDetail.buyNumber*orderDetail.insPrice
+            // console.log(shoppingCartList[i].ins.insName)
+            _this.orderDetailList.push(orderDetail)
+          }
+          console.log(_this.orderDetailList)
+          const formData = new FormData
+          let orderDetailListToString = JSON.stringify(_this.orderDetailList)
+          formData.append('orderDetailListToString', orderDetailListToString)
+          axios({
+            method: 'post',
+            url: '/order/addOrderDetail',
+            data: formData,
+            headers: {
+              'ContentType': 'multipart/form-data',
+            }
+          }).then((resp) => {
+            if (resp.data.code === 10000) {
+              _this.getShoppingCartList(_this.pageNum, _this.pageSize)
+            }
+            if (resp.data.code === 10001) {
+              _this.$message({
+                showClose: true,
+                message: resp.data.msg,
+                type: 'error'
+              });
+            }
+          });
+
+          _this.$message({
+            showClose: true,
+            message: resp.data.msg,
+            type: 'success'
+          });
+
+        }
+        if (resp.data.code === 10001) {
+          _this.$message({
+            showClose: true,
+            message: resp.data.msg,
+            type: 'error'
+          });
+        }
+      });
+    },
   },
   created() {
     const _this = this
@@ -240,6 +438,17 @@ export default {
       //获取购物车列表
       _this.getShoppingCartList(_this.pageNum, _this.pageSize)
 
+      //获取收货地址
+      axios({
+        method: 'get',
+        url: '/userAddr/list',
+        params: {
+          userId: _this.form.userId
+        }
+      }).then((resp) => {
+        _this.userAddr = resp.data.data
+      });
+
     } else {
       this.$router.push('/UserLogin');
       _this.$message({
@@ -248,14 +457,18 @@ export default {
         message: '您未登录，请先登录'
       });
     }
-  },
+  }
 }
 </script>
 
 <style scoped>
-#shoppingCartList {
+#shoppingCartList, #buyerInfo {
   width: 1200px;
   margin: 75px auto;
 
+}
+
+#buyerInfo {
+  padding-bottom: 75px;
 }
 </style>
